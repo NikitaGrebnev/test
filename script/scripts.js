8f1ee54bea214e88d8bb7efbbbcad91d4a627dca
Nekit <nikitagrebnev6@gmail.com>
2018-11-05 16:26:39 +0300

    moy perviy commit

---
+++ script/scripts.js
@@ -0,0 +1,19 @@
+﻿budget = prompt("Ваш бюджет",0);
+shopname = prompt('Название магазина');
+
+var mainlist = {
+	budg: budget, shop: shopname,
+	shopgoods: [], employers: {}
+}
+
+var q1 = prompt("Какой тип товаров будем продавать?");
+var q2 = prompt("Какой тип товаров будем продавать?");
+var q3 = prompt("Какой тип товаров будем продавать?");
+
+mainlist.shopgoods[0] = q1;
+mainlist.shopgoods[1] = q2;
+mainlist.shopgoods[2] = q3;
+
+budgPerDay = alert("Бюджет на день равен " + budget/30);
+
+document.write(mainlist.shopgoods);
